810dcbf31d36e83fd20e2beeb10fa551ebb100cc
Ashwini Tiwari <ashwini12tiwari@gmail.com>
2026-09-15 21:01:13 +0530

    dd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
   production: true,
   earnivo: {
     apiBaseUrl: "https://api.admobility.in/api",
-    apiKey: "ak_6375bbe8747ecf38999bda63fa7c0bae6270c510ae9bddaa",
+    apiKey: "ak_9b504d51209aae38bc92fc7a1f1cda10bd9d9178c798ab93",
   },
 } as const;
