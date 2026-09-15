4167992e74d6d5d2395bd2871ac88a7114ef9c25
Ashwini Tiwari <ashwini12tiwari@gmail.com>
2026-09-15 21:56:06 +0530

    add

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { ToolSearch } from './shared/components/tool-search/tool-search';
 import { ThemeService } from './core/services/theme.service';
+import { EarnivoReward } from './shared/components/earnivo-reward/earnivo-reward';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, ToolSearch],
+  imports: [RouterOutlet, Header, Footer, ToolSearch, EarnivoReward],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
